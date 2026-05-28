caa4c32747703b48f734fe567c7f315eea21c887
unknown <germano.econo23@gmail.com>
2026-05-28 19:14:46 -0300

    Organizando pastas do projeto

--- Gestaohospitalar/android/app/src/main/kotlin/com/example/gestaohospitalar01/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gestaohospitalar01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ Gestaohospitalar/lib/domain/entities/paciente.dart
@@ -0,0 +1,19 @@
+//classe e seus atributos
+class Paciente {
+  int? id;
+  String? nome;
+  int? cpf;
+  String? sexo;
+  DateTime? nascimento;
+  String? alergias;
+  String? tipoSanguineo;
+  String? historicoClinico;
+  int? telefone;
+  String? rua;
+	String? numeroCasa;
+	String? bairro;
+	String? cidade;
+	String? estado;
+	int? cep;
+	String? nomeResponsavel;
+}
